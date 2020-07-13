fa0af4cf4e66e61690960501a0192f757289e16f
Amanda <amandaralves@live.com>
2020-07-12 21:33:27 -0300

    Layout Pages

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    <h2>Hello, World!</h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts/{post}', 'PostsController@show');
+//Route::get('/posts/{post}', 'PostsController@show');
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
